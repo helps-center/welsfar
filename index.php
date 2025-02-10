2f8c19206e1a286fe18e93091a3b34b9da446424
helps-center <ajeetkumarshukla820@gmail.com>
2025-02-10 20:29:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -225,13 +225,13 @@
 
 
 <!-- Begin Web-Stat code v 7.0 -->
-<span id="wts2193119"></span>
+<span id="wts2193670"></span>
 <script>var wts=document.createElement('script');wts.async=true;
 wts.src='https://app.ardalio.com/log7.js';document.head.appendChild(wts);
-wts.onload = function(){ wtslog7(2193119,5); };
+wts.onload = function(){ wtslog7(2193670,1); };
 </script><noscript><a href="https://www.web-stat.com">
-<img src="https://app.ardalio.com/7/5/2193119.png" 
-alt="Web-Stat web traffic analysis"></a></noscript>
+<img src="https://app.ardalio.com/7/1/2193670.png" 
+alt="Web-Stat traffic analysis"></a></noscript>
 <!-- End Web-Stat code v 7.0 -->
 
 </body>
